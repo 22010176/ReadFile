8cf652a0e3f068eaa131ebea248551366e49c9ec
____ <22010176@st.phenikaa-uni.edu.vn>
2023-06-29 15:42:08 +0700

    ed

--- png/c/png/ReadPNG.c
+++ png/c/png/ReadPNG.c
@@ -69,16 +69,17 @@ int  GetIdatSize(Chunk** d) {
 }
 char* InflateData(char* data, int len, int* Psize) {
   char* output;
-  int output_size = Psize[0] * Psize[1] * 20;
+  int output_size = Psize[0] * Psize[1] * 4 + Psize[1];
   do { output = (char*)malloc(output_size); } while (!output);
+
   z_stream  out;
   out.zalloc = Z_NULL;
   out.zfree = Z_NULL;
   out.opaque = Z_NULL;
 
-  out.avail_in = (uInt)(len);
+  out.avail_in = (uInt)(len + 1);
   out.next_in = (Bytef*)data;
-  out.avail_out = (uInt)(output_size);
+  out.avail_out = (uInt)(output_size + 1);
   out.next_out = (Bytef*)output;
 
   inflateInit(&out);
@@ -91,25 +92,27 @@ char* PackedData(Chunk** data, int len) {
   char* _data;
   int j = 0, size = GetIdatSize(data);
   do { _data = (char*)malloc(size + 1); } while (!_data);
+
   for (int i = 0;i < len;i++) {
     if (strcmp(data[i]->name, "IDAT")) continue;
     Chunk* x = data[i];
     memcpy(_data + j, x->data, x->size);
     j += x->size;
   }
-  _data[len] = '\0';
+  _data[size] = '\0';
   return _data;
 }
+int calcDataLen(int* size) { return size[1] * size[0] * 4 + size[1]; }
 int* GetPNGScale(Chunk* IHDR) {
   int size[2] = { GetIntData(IHDR->data, 4),GetIntData(IHDR->data + 4, 4) };
   return (int*)memcpy(malloc(2 * sizeof(int)), size, 2 * sizeof(int));
 }
 char* Decompress(Chunk** data) {
   Chunk** idat = GetIdat(data);
-  int* size = GetPNGScale(data[1]), _d = GetIdatSize(data), _l = GetLen((void**)idat);
+  int* size = GetPNGScale(data[1]);
 
-  char* _data = PackedData(idat, _l);
-  char* output = InflateData(_data, _d, size);
+  char* _data = PackedData(idat, GetLen((void**)idat));
+  char* output = InflateData(_data, GetIdatSize(data), size);
 
   free(idat);
   free(size);
@@ -122,21 +125,35 @@ void WritePNGData(FILE* _dest, Chunk** data) {
   for (int i = 0; i < len;i++) data[i]->FPrint(data[i], _dest, 1);
 }
 
-
+void FPrintPNGData(FILE* _dest, char* output, int* size) {
+  int indent = size[0] * size[1] * 4 + 1;
+  for (int i = 0; i < size[0] * size[1] * 4 + size[1];i++) {
+    fprintf(_dest, "%5d%c", GetReal(output[i], 8), AddIndent(i + 1, indent));
+  }
+}
 #if __INCLUDE_LEVEL__ == 0
 int main() {
   char a[] = "C:/Users/ducmi/Downloads/New folder/data/png/sample.png";
   char b[] = "Ext.txt";
+  char c[] = "Ext2.txt";
   FILE* t = fopen(a, "rb");
-  FILE* o = fopen(b, "wb");
+  // FILE* o = fopen(b, "wb");
+  // FILE* ff = fopen(c, "wb+");
+
   Chunk** _a = GetAllChunk(t, 2000000000);
-  WritePNGData(o, _a);
-  // _a[1]->Print(_a[1], 1);
+  // WritePNGData(ff, _a);
+  _a[1]->Print(_a[1], 1);
+
+  int* size = GetPNGScale(_a[1]);
+  char* e = Decompress(_a);
+
+  int indent = size[0] * 4 + 1;
+  for (int i = 0; i < calcDataLen(size);i++) {
+    printf("%4d%c", GetReal(e[i], 8), AddIndent(i + 1, indent));
+  }
+
   for (int i = 0; _a[i] != (Chunk*)-1;i++) _a[i]->Free(_a[i]);
   free(_a);
-  char* e = Decompress(_a);
-  for (int i = 0; i < 1000;i++) printf("%3d ", e[i]);
-  // free(size);
   free(e);
 }
 #else
