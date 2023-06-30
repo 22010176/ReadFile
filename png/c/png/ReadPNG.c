1bfaacb40c7ff1b08968ff22d4e3b0b6d4c057a6
____ <22010176@st.phenikaa-uni.edu.vn>
2023-06-30 10:35:42 +0700

    ed

--- png/c/png/ReadPNG.c
+++ png/c/png/ReadPNG.c
@@ -114,30 +114,101 @@ char* Decompress(Chunk** data) {
 }
 void WritePNGData(FILE* _dest, Chunk** data) {
   int len = GetLen((void**)data);
-  for (int i = 0; i < len;i++) data[i]->FPrint(data[i], _dest, 1);
+  for (int i = 0; i < len;i++) {
+    data[i]->FPrint(data[i], _dest, 1);
+  }
+  fflush(_dest);
 }
 
 void FPrintPNGData(FILE* _dest, char* output, int* size) {
   int indent = size[0] * 4 + 1, len = calcDataLen(size);
   for (int i = 0; i < len;i++) fprintf(_dest, "%2x%c", GetReal(output[i], 8), AddIndent(i + 1, indent));
+  fflush(_dest);
+}
+char** Create2dArr(int x, int y) {
+  char** res;
+  do { res = (char**)malloc(y); } while (!res);
+  for (int i = 0; i < y;i++) {
+    do {
+      res[i] = (char*)malloc(x + 1);
+      printf("%d %d %d\n", i, y, !res[i]);
+    } while (!res[i]);
+  }
+  return res;
 }
 char** _1Dto2D(char* arr, int l, int* size) {
-  char** res = (char**)malloc(size[1]);
-  for (int i = 0; i < size[0];i++) {
-
+  int _row = size[0] * 4 + 1;
+  char** res = Create2dArr(_row, size[1]);
+  for (int i = 0; i < size[1];i++) for (int j = 0; j < _row;j++) res[i][j] = arr[_row * i + j];
+  return res;
+}
+int None(int a, int b, int c, int x) { return x; }
+int Sub(int a, int b, int c, int x) { return (x + a) % 256; }
+int Up(int a, int b, int c, int x) { return (x + b) % 256; }
+int Average(int a, int b, int c, int x) { return (x + (int)floor((a + b) / 2)) % 256; }
+int Paeth(int a, int b, int c, int x) {
+  int p = a + b - c;
+  int pa = abs(p - a), pb = abs(p - b), pc = abs(p - c);
+  if (pa <= pb && pa <= pc) return pa;
+  if (pb <= pc) return pc;
+  return (x + pc) % 256;
+}
+int(*PickFilter(int t))(int, int, int, int) {
+  switch (t) {
+  case 0: return None;
+  case 1: return Sub;
+  case 2: return Up;
+  case 3: return Average;
+  case 4: return Paeth;
+  default: (int(*)(int, int, int, int)) - 1;
   }
 }
+
 #if __INCLUDE_LEVEL__ == 0
 int main() {
   char a[] = "C:/Users/ducmi/Downloads/New folder/data/png/sample.png";
 
   FILE* t = fopen(a, "rb");
+  FILE* z = fopen("aeee.txt", "w");
   Chunk** _a = GetAllChunk(t);
-  char* e = Decompress(_a);
-  // FPrintPNGData(fopen("a.txt", "wb"), e, GetPNGScale(_a[1]));
-  for (int i = 0; _a[i] != (Chunk*)-1;i++) _a[i]->Free(_a[i]);
 
+  WritePNGData(z, _a);
+  char* e = Decompress(_a);
+  int* size = GetPNGScale(_a[1]);
+
+  for (int i = 0; _a[i] != (Chunk*)-1;i++) _a[i]->Free(_a[i]);
   free(_a);
+
+  char** d = _1Dto2D(e, calcDataLen(size), size);
+  printf("HE");
+  // char** d2 = Create2dArr(size[0] * 4 + 1, size[1]);
+
+  // for (int i = 0; i < size[1];i++) {
+  //   int (*f)(int, int, int, int) = PickFilter(d[i][0]);
+  //   for (int j = 1; j < size[0] * 4 + 1;j++) {
+
+  //   }
+  // }
+
+
+  for (int i = 0; i < size[1];i++) {
+    for (int j = 0; j < 4;j++) printf("%4d", GetReal(e[i * (size[0] * 4 + 1) + j], 8));
+    printf("\n");
+    // for (int j = 1; j < size[0];j++) {
+    //   for (int k = 0; k < 4;k++)printf("%x", GetReal(d[i][j + k], 8));
+    // }
+    // printf("\n");
+  }
+  // for (int i = 0;i < size[1];i++) {
+  //   free(d[i]);
+  //   free(d2[i]);
+  // }
+  fclose(t);
+  fclose(z);
+  // free(d);
+  // free(d2);
+
+  free(size);
   free(e);
 }
 #else
